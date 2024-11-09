b5498365d0766deb25cf405d0af72a6766e58b78
lsurma <kontakt@lsurma.pl>
2024-11-09 23:43:37 +0100

    wip

--- package/Surma.Translations/Surma.Translations/assets/main.js
+++ package/Surma.Translations/Surma.Translations/assets/main.js
@@ -1,29 +1,24 @@
-import {Button, CompoundButton, CompoundButtonDefinition, ButtonDefinition} from '@fluentui/web-components';
+import {Button, CompoundButton, Accordion, AccordionItem, MenuItem, Menu, MenuButton} from '@fluentui/web-components';
 import { FluentDesignSystem } from '@fluentui/web-components';
-import { FASTElement } from '@microsoft/fast-element';
 import { setTheme } from '@fluentui/web-components';
-import { webLightTheme } from '@fluentui/tokens';
+import { webLightTheme, webDarkTheme } from '@fluentui/tokens';
+
+const fluent3 = {
+  setTheme: setTheme,
+  webLightTheme: webLightTheme,
+  webDarkTheme: webDarkTheme
+}
+
+window.fluent3 = fluent3;
+
+// Remap types
+const components = [Button, CompoundButton, Accordion, AccordionItem, MenuItem, Menu, MenuButton];
+const prefix = 'fluent3-';
+components.forEach(component => {
+  const tagName = component.name.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
+  const newTagName = prefix + tagName;  
+  FluentDesignSystem.registry.define(newTagName, component);
+});
 
 setTheme(webLightTheme);
 
-
-export class CustomFluentCompoundButton extends CompoundButton {
-    constructor() {
-        super();
-    }
-    }
-
-FluentDesignSystem.registry.define('custom-button', Button);
-
-    
-// FASTElement.compose(CustomButton, {
-//     ...ButtonDefinition,
-//     name: 'custom-button',
-// });
-// FluentDesignSystem.registry.define('custom-button', CustomButton);
-
-FASTElement.compose(CustomFluentCompoundButton, {
-    ...CompoundButtonDefinition,
-    name: 'custom-comp-button',
-});
-FluentDesignSystem.registry.define('custom-comp-button', CustomFluentCompoundButton);
